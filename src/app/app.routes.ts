8e1adb14d354d798a07c2bc8625c8da346c24a82
Furkan <furkan61kcmn@gmail.com>
2025-01-22 21:35:56 +0300

    add chat page and sidebar components with Tailwind CSS integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ChatPageComponent } from './chat-page/chat-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: ChatPageComponent }];
