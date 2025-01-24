a8757040b5caeebce2d0727d40c815b8b9027c45
Furkan <furkan61kcmn@gmail.com>
2025-01-24 15:00:48 +0300

    implement chat page layout with sidebar, message display, and input textbox

--- src/app/chat-page/chat-page.component.ts
+++ src/app/chat-page/chat-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ChatTextboxComponent } from '../../components/chat-textbox/chat-textbox.component';
+import { ChatMessageComponent } from '../../components/chat-message/chat-message.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-chat-page',
   standalone: true,
-  imports: [],
+  imports: [ChatTextboxComponent, ChatMessageComponent, SidebarComponent],
   templateUrl: './chat-page.component.html',
-  styleUrl: './chat-page.component.css'
+  styleUrl: './chat-page.component.css',
 })
 export class ChatPageComponent {
-
+  currentMessage: string = 'lorem ipsum dolor sit amet';
 }
---
+++ src/components/chat-textbox/chat-textbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-textbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-textbox.component.html',
+  styleUrl: './chat-textbox.component.css',
+})
+export class ChatTextboxComponent {}
